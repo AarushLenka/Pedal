e31e9f1a08c5f51617270f9dd5527f116542bf2b
lenkaaarush <lenkaaarush@gmail.com>
2025-12-22 15:10:38 +0000

    theme change

---
+++ app/src/main/java/com/example/pedal/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pedal
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // 3 second delay
+    }
+}
